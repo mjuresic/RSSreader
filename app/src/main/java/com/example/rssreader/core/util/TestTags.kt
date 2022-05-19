7d4e8d507db360e9d73031e82ca76fb29ee91c34
Marijan <mjuresic@gmail.com>
2022-05-19 12:48:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/rssreader/core/util/TestTags.kt
@@ -0,0 +1,7 @@
+package com.example.rssreader.core.util
+
+object TestTags {
+    const val TITLE_TEXT_FIELD = "TITLE_TEXT_FIELD"
+    const val URL_TEXT_FIELD = "CONTENT_TEXT_FIELD"
+    const val FEED_ITEM = "NOTE_ITEM"
+}
